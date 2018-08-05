fe30bccceaeaf376964bed655a516bff3507591d
Kanhaiya Thakur <kanthaku1@publicisgroupe.net>
2018-08-05 19:04:12 +0530

    Adding cloud functions

--- src/js/service.js
+++
@@ -1,5 +0,0 @@
-export default function createHTMLElement(htmlString) {
-  const template = document.createElement('template');
-  template.innerHTML = htmlString;
-  return template.content.firstElementChild;
-}
